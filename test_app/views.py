6d807c27593fbe404e92995d67e051beccf51389
deepakTreeShadeBooks <deepak@treeshadebooks.com>
2025-08-19 17:41:40 +0530

    done

--- test_app/views.py
+++ test_app/views.py
@@ -1,13 +1,15 @@
 
 from django.shortcuts import render, redirect
 from .forms import ParagraphForm
+from .models import Paragraph
 
 def paragraph_form_view(request):
+	messages = Paragraph.objects.order_by('-created_at')
 	if request.method == 'POST':
 		form = ParagraphForm(request.POST)
 		if form.is_valid():
 			form.save()
-			return render(request, 'form.html', {'form': ParagraphForm(), 'success': True})
+			return render(request, 'form.html', {'form': ParagraphForm(), 'success': True, 'messages': messages})
 	else:
 		form = ParagraphForm()
-	return render(request, 'form.html', {'form': form})
+	return render(request, 'form.html', {'form': form, 'messages': messages})
